4e465731353facadf645cc64347f4ba2a37da805
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-06-17 15:59:48 +0200

    Rspec support

---
+++ lib/ElmerFudd/rspec.rb
@@ -0,0 +1,74 @@
+class ElmerFudd::FakeProducer
+  def initialize(_connection = :no_connection)
+    @casts = Hash.new do |hash, queue_name|
+      hash[queue_name] = []
+    end
+
+    @notifications = Hash.new do |hash, exchange|
+      hash[exchange] = Hash.new do |hash, event_name|
+        hash[event_name] = []
+      end
+    end
+  end
+
+  def call(queue_name, payload, timeout: 10)
+    # not implemented, behaving like other side does not respond
+    raise Timeout::Error
+  end
+
+  def cast(queue_name, payload)
+    @casts[queue_name] << payload
+  end
+
+  def notify(exchange, event_name, payload)
+    @notifications[exchange][event_name] << payload
+  end
+
+  def clear!
+    initialize(nil)
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    ObjectSpace.each_object(ElmerFudd::FakeProducer, &:clear!)
+  end
+end
+
+RSpec::Matchers.define :have_received_cast_on do |queue_name|
+  match do |rabbit|
+    queue(rabbit).size == (@count || 1)
+  end
+
+  chain :with_payload do |payload|
+    @payload = payload
+  end
+
+  chain :times do |times|
+    @count = times
+  end
+
+  define_method :queue do |server|
+    queue = server.instance_eval { @casts }[queue_name]
+    if @payload
+      queue = queue.select { |payload| payload == @payload }
+    end
+    queue
+  end
+
+  failure_message_for_should do |rabbit|
+    if @count
+      "expected a message on '#{queue_name}' #{@count} times, but received #{queue(rabbit).size} times"
+    else
+      "expected a message on '#{queue_name}' 1 time, but received #{queue(rabbit).size} times"
+    end
+  end
+
+  failure_message_for_should_not do |rabbit|
+    "expected no message on '#{queue_name}', but receved at least one"
+  end
+
+  description do
+    "receive a message on '#{queue_name}'"
+  end
+end
