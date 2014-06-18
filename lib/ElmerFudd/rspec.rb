94f33f00b098a5abf4304037c49bfc9a98134c6f
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-06-18 14:18:55 +0200

    matcher for notify

--- lib/ElmerFudd/rspec.rb
+++ lib/ElmerFudd/rspec.rb
@@ -72,3 +72,43 @@ RSpec::Matchers.define :have_received_cast_on do |queue_name|
     "receive a message on '#{queue_name}'"
   end
 end
+
+RSpec::Matchers.define :have_received_notification_on do |exchange_name, options|
+  routing_key = options.fetch(:routing_key)
+
+  match do |rabbit|
+    queue(rabbit).size == (@count || 1)
+  end
+
+  chain :with_payload do |payload|
+    @payload = payload
+  end
+
+  chain :times do |times|
+    @count = times
+  end
+
+  define_method :queue do |server|
+    queue = server.instance_eval { @notifications }[exchange_name][routing_key]
+    if @payload
+      queue = queue.select { |payload| payload == @payload }
+    eeend
+    queue
+  end
+
+  failure_message_for_should do |rabbit|
+    if @count
+      "expected a message on '#{exchange_name}' with routing key '#{routing_key}' #{@count} times, but received #{queue(rabbit).size} times"
+    else
+      "expected a message on '#{exchange_name}' with routing key '#{routing_key}' 1 time, but received #{queue(rabbit).size} times"
+    end
+  end
+
+  failure_message_for_should_not do |rabbit|
+    "expected no message on '#{queue_name}', but receved at least one"
+  end
+
+  description do
+    "receive a message on '#{queue_name}'"
+  end
+end
