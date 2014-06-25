b72d921e5ca36f1a85835996266c546a91968894
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-06-25 11:23:27 +0200

    Fix typo

--- lib/ElmerFudd.rb
+++ lib/ElmerFudd.rb
@@ -191,12 +191,12 @@ module ElmerFudd
     def self.call(env, message, filters)
       call_next(env, message, filters)
     rescue Exception => e
-      Airbrake.notify(e, parametets: {
+      Airbrake.notify(e, parameters: {
                         payload: message.payload,
                         queue: message.route.queue_name,
                         exchange_name: message.route.exchange_name,
                         routing_key: message.delivery_info.routing_key,
-                        matched_routing_key: message.route.routing_key
+                        matched_routing_key: message.route.routing_keys
                       })
       raise
     end
