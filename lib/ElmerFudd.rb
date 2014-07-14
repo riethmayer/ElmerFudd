b91bdbd806bb4681a86ea66057d4bdbe76a8f102
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-07-14 11:08:01 +0200

    handling redirect of message on exception.

--- lib/ElmerFudd.rb
+++ lib/ElmerFudd.rb
@@ -230,6 +230,27 @@ module ElmerFudd
     end
   end
 
+  class RedirectFailedFilter
+    include Filter
+    def initialize(producer, error_queue, exception: Exception,
+                   exception_message_matches: /.*/)
+      @producer = producer
+      @error_queue = error_queue
+      @exception = exception
+      @exception_message_matches = exception_message_matches
+    end
+
+    def call(env, message, filters)
+      call_next(env, message, filters)
+    rescue @exception => e
+      if e.message =~ @exception_message_matches && retry_num < @times
+        @producer.cast @error_queue, message.payload
+      else
+        raise
+      end
+    end
+  end
+
   class RetryFilter
     include Filter
 
