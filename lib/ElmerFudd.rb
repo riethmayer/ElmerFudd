36033873afbe3e0b354081c143ff629a93829462
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-07-15 17:28:21 +0200

    Specify exchange properties

--- lib/ElmerFudd.rb
+++ lib/ElmerFudd.rb
@@ -151,7 +151,7 @@ module ElmerFudd
 
   class TopicHandler < DirectHandler
     def exchange(env)
-      env.channel.topic(@route.exchange_name)
+      env.channel.topic(@route.exchange_name, durable: true, internal: false, autodelete: false)
     end
   end
 
