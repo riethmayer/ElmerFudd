a373d4d8a27df04d552a38e0c5a8d62cbb2e4a4b
Artur Roszczyk <artur.roszczyk@gmail.com>
2014-07-23 15:53:27 +0200

    Configurable DropFailedFilter

--- lib/ElmerFudd.rb
+++ lib/ElmerFudd.rb
@@ -177,13 +177,28 @@ module ElmerFudd
   end
 
   class DropFailedFilter
-    extend Filter
+    include Filter
+
     def self.call(env, message, filters)
+      new.call(env, message, filters)
+    end
+
+    def initialize(exception: Exception,
+                   exception_message_matches: /.*/)
+      @exception = exception
+      @exception_message_matches = exception_message_matches
+    end
+
+    def call(env, message, filters)
       call_next(env, message, filters)
-    rescue Exception => e
-      env.logger.info "Ignoring failed payload: #{message.payload}"
-      env.logger.debug "#{e.class}: #{e.message}"
-      e.backtrace.each { |l| env.logger.debug(l) }
+    rescue @exception => e
+      if e.message =~ @exception_message_matches
+        env.logger.info "Ignoring failed payload: #{message.payload}"
+        env.logger.debug "#{e.class}: #{e.message}"
+        e.backtrace.each { |l| env.logger.debug(l) }
+      else
+        raise
+      end
     end
   end
 
