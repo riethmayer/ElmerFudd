a7ba7150acdbcf1cbe9743adcaa44ac1cfff4789
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2015-05-18 14:53:28 +0200

    using logger

--- lib/ElmerFudd/publisher.rb
+++ lib/ElmerFudd/publisher.rb
@@ -1,23 +1,28 @@
 module ElmerFudd
   class Publisher
-    def initialize(connection, uuid_service: -> { rand.to_s })
+    def initialize(connection, uuid_service: -> { rand.to_s }, logger: Logger.new($stdout))
+      puts "LOGGGER #{logger}"
       @connection = connection
+      @logger = logger
       @uuid_service = uuid_service
       @topic_x = {}
     end
 
     def notify(topic_exchange, routing_key, payload)
+      @logger.debug "ElmerFudd: NOTIFY - topic_exchange: #{topic_exchange}, routing_key: #{routing_key}, payload: #{payload}"
       @topic_x[topic_exchange] ||= channel.topic(topic_exchange)
       @topic_x[topic_exchange].publish payload.to_s, routing_key: routing_key
       nil
     end
 
     def cast(queue_name, payload)
+      @logger.debug "ElmerFudd: CAST - queue_name: #{queue_name}, payload: #{payload}"
       x.publish(payload.to_s, routing_key: queue_name)
       nil
     end
 
     def call(queue_name, payload, timeout: 10)
+      @logger.debug "ElmerFudd: CALL - queue_name: #{queue_name}, payload: #{payload}, timeout: #{timeout}"
       mutex = Mutex.new
       resource = ConditionVariable.new
       correlation_id = @uuid_service.call
